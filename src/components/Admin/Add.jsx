30df93f1daf326298f08a46bc3a8e4921dd201e4
woozyfr <alex@woozy.fr>
2021-02-04 13:53:46 +0100

    add quick forms for add

--- src/components/Admin/Add.jsx
+++ src/components/Admin/Add.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "./css/styles.css";
 import PropTypes from "prop-types";
@@ -9,6 +9,9 @@ import axios from "axios";
  */
 
 export default function Add(props) {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [isSelected, setIsSelected] = useState(false);
   const [forms, setFroms] = useState({
     name: null,
     description: null,
@@ -36,24 +39,41 @@ export default function Add(props) {
 
     event.preventDefault();
   };
-  const detectLatLong = (event) => {
-    axios
-      .get(
-        `https://api-adresse.data.gouv.fr/search/?q=${encodeURI(
-          forms.address + " " + forms.city
-        )}&postcode=${forms.code_postal}`
-      )
-      .then((res) => {
-        if (Array.isArray(res.data.features)) {
-          setFroms({
-            ...forms,
-            latitude: res.data.features[0].geometry.coordinates[1],
-            longitude: res.data.features[0].geometry.coordinates[0],
-          });
-        }
-      });
-    event.preventDefault();
+
+  useEffect(() => {
+    if (search.length > 8 && !isSelected) {
+      axios
+        .get(`https://api-adresse.data.gouv.fr/search/?q=${encodeURI(search)}`)
+        .then((res) => {
+          console.log(res);
+          if (Array.isArray(res.data.features)) {
+            setResults(res.data.features);
+          }
+        });
+    } else {
+      console.log("Pas assez de caracteres");
+    }
+  }, [search.length, isSelected]);
+
+  const searchChange = (event) => {
+    setSearch(event.target.value);
+    setIsSelected(false);
   };
+
+  const selectAddress = (result) => {
+    setFroms({
+      ...forms,
+      latitude: result.geometry.coordinates[1],
+      longitude: result.geometry.coordinates[0],
+      city: result.properties.city,
+      address: result.properties.name,
+      code_postal: result.properties.postcode,
+    });
+    setSearch(result.properties.label);
+    setResults([]);
+    setIsSelected(true);
+  };
+
   return (
     <div>
       <h1>Ajout d'un nouveau Bar</h1>
@@ -80,70 +100,6 @@ export default function Add(props) {
             value={forms.description}
           />
         </div>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="address"
-            name="address"
-            placeholder="Adresse (rue etc...)"
-            onChange={handleChange}
-            value={forms.address}
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="code_postal"
-            name="code_postal"
-            placeholder="Code Postal"
-            onChange={handleChange}
-            value={forms.code_postal}
-          />
-        </div>
-
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="city"
-            placeholder="Ville"
-            name="city"
-            onChange={handleChange}
-            value={forms.city}
-          />
-        </div>
-        <button
-          type="button"
-          className="btn btn-success"
-          onClick={detectLatLong}
-        >
-          Detect latitude/longitude
-        </button>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="latitude"
-            name="latitude"
-            placeholder="latitude"
-            onChange={handleChange}
-            value={forms.latitude}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            className="form-control"
-            id="longitude"
-            name="longitude"
-            placeholder="longitude"
-            onChange={handleChange}
-            value={forms.longitude}
-          />
-        </div>
 
         <div className="form-group">
           <input
@@ -157,9 +113,100 @@ export default function Add(props) {
           />
         </div>
 
-        <button type="submit" class="btn btn-primary">
-          Envoyer
-        </button>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            id="search"
+            name="search"
+            placeholder="Adresse a chercher (6 caractere minimum)"
+            onChange={searchChange}
+            value={search}
+          />
+        </div>
+        <div>
+          {results.map((result) => {
+            return (
+              <div
+                key={result.properties.label}
+                onClick={() => selectAddress(result)}
+                className="rowAutocomplete"
+              >
+                <div> {result.properties.label}</div>
+              </div>
+            );
+          })}
+        </div>
+        <div class="text-center">
+          <button type="submit" class="btn btn-primary">
+            Envoyer
+          </button>
+        </div>
+        <h5>Debug mode (hidden normalement)</h5>
+
+        <div>address : {forms.address}</div>
+        <div>code_postal : {forms.code_postal}</div>
+        <div>city : {forms.city}</div>
+        <div>latitude : {forms.latitude}</div>
+        <div>longitude : {forms.longitude}</div>
+        <div className="form-group">
+          <input
+            type="hidden"
+            className="form-control"
+            id="address"
+            name="address"
+            placeholder="Adresse (rue etc...)"
+            onChange={handleChange}
+            value={forms.address}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="hidden"
+            className="form-control"
+            id="code_postal"
+            name="code_postal"
+            placeholder="Code Postal"
+            onChange={handleChange}
+            value={forms.code_postal}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="hidden"
+            className="form-control"
+            id="city"
+            placeholder="Ville"
+            name="city"
+            onChange={handleChange}
+            value={forms.city}
+          />
+        </div>
+
+        <div className="form-group">
+          <input
+            type="hidden"
+            className="form-control"
+            id="latitude"
+            name="latitude"
+            placeholder="latitude"
+            onChange={handleChange}
+            value={forms.latitude}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="hidden"
+            className="form-control"
+            id="longitude"
+            name="longitude"
+            placeholder="longitude"
+            onChange={handleChange}
+            value={forms.longitude}
+          />
+        </div>
       </form>
     </div>
   );
